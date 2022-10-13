53ee524b1b1cd4ee59cebf403f401bc2b07aacf8
Chelsea Pae <chelseapae@gmail.com>
2022-10-12 18:48:01 -0700

    Create test for rendering component

--- client/src/Reviews/ReviewIndex.test.js
+++ client/src/Reviews/ReviewIndex.test.js
@@ -1,6 +1,23 @@
-import { render } from '@testing-library/react'
-
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import Reviews from './ReviewIndex.jsx';
+// const server = setupServer(
+//   rest.get('/reviews', (req, res, ctx) => {
+//     return res(ctx.status(200))
+//   })
+// )
+
+// beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+// afterAll(() => server.close())
+
+describe('Ratings and Reviews', function () {
+  it('should render Ratings & Reviews', async () => {
+    render(<Reviews />);
+    expect(screen.getByText('RATINGS & REVIEWS')).toBeDefined();
+  });
+});
 
 test('test runs', async () => {
     expect(1 + 1).toEqual(2)

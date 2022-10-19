93617092412dc576ea4915150aa0dfc6492e87af
netbek932 <rjeren22@gmail.com>
2022-10-18 21:24:19 -0400

    Added content and inputs on modal, only close functional

--- client/src/QuestionsAnswers/components/AddAnswer.jsx
+++ client/src/QuestionsAnswers/components/AddAnswer.jsx
@@ -3,23 +3,42 @@ import { useState } from 'react';
 const axios = require('axios');
 
 const AddAnswer = ({show, onClose, question, productId}) => {
-  const [modal, setModal] = useState(show)
+  const [modal, setModal] = useState(show);
 
   const handleClose = (e) => {
     e.preventDefault();
     onClose(e);
   }
 
+  const handleChange = (e, field) => {
+
+  }
+
   if (!show) {
     return null;
   } else {
     return (
-      <div className="answerModal">
+      <form className="answerModal">
         <h2>Submit your Answer</h2>
         {/* <h3>{Product Name will go here}</h3> */}
         <h4>{question.question_body}</h4>
+
+        <div><label className="youranswer"> Your Answer * </label></div>
+          <textarea type="text" maxLength="1000" required rows={10} cols={50} onChange={(e) => handleChange(e, 'body')}/>
+
+        <div><label className="yournickname"> What is your nickname? * </label></div>
+          <input type="text" maxLength="60" placeholder="Example: jack543!" required onChange={(e) => handleChange(e, 'answerer_name')}/>
+          <div><small> For privacy reasons, do not use your full name or email address</small></div>
+
+        <div><label className="youremail"> What is your email? * </label></div>
+          <input type="text" maxLength="60" placeholder="Example: jack@email.com" required onChange={(e) => handleChange(e, 'email')}/>
+        <div><small> For authentication reasons, you will not be emailed </small></div>
+
+        {/* Option to upload photos will go here */}
+
+        <button>Submit</button>
         <button onClick={(e) => handleClose(e)}>Close</button>
-      </div>
+      </form>
     )
   }
 }

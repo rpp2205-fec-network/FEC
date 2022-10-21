263a0d4660d7a8378b891af6d6980de19db46a50
Zachary Atha <zacharyatha@gmail.com>
2022-10-21 17:08:17 -0400

    made add to cart button reset when style is selected

--- client/src/Product Overview/components/addToCart.jsx
+++ client/src/Product Overview/components/addToCart.jsx
@@ -20,40 +20,40 @@ export default class AddToCart extends React.Component {
         this.onOptionClick = this.onOptionClick.bind(this);
     }
 
-    componentDidUpdate() {
-        if (this.props.currentStyleInfo !== undefined) {
-            if (this.state.size === '-1' || undefined) {
-                var data = [];  
-                var skusArray = this.props.currentStyleInfo.skus
-                var quantityArray = [];
-                for (var key in skusArray) {
-                    data.push({sku: key, quantity: skusArray[key].quantity, size: skusArray[key].size})
-                }
-                console.log('IMPORTANT DATA', this.props.currentStyleInfo.skus, data[0])
-                for (var i = 1; i <= this.props.currentStyleInfo.skus[data[0].sku].quantity; i++) {
-                    quantityArray.push(i)
-                }
-                if (quantityArray.length > 15) {
-                    quantityArray.length = 15;
-                }
-                this.setState({
-                    size: this.props.currentStyleInfo.skus[data[0].sku].size,
-                    quantity: 1,
-                    totalQuantity: this.props.currentStyleInfo.skus[data[0].sku].quantity,
-                    quantityArr: quantityArray,
-                    data: data,
-                    currentSku: data[0].sku
-                })
-            } else if (this.state.flag === true) {
-                //this should be the case for when state changes
-                console.log('second part of componentDidUpdate')
-                var newQuantityTotal = 0
-                var newQuantityArray = []
-                this.setState({
-                    flag: false
-                })
-
+    componentDidUpdate(prevProps, prevState) {
+        console.log('PREVPROPS,', prevProps,'\ncurrentProps', this.props, '\n PrevState', prevState, '\n CurrentState', this.state)
+        //if new props are received
+        if (prevProps.currentStyleInfo !== this.props.currentStyleInfo) {
+            var data = [];  
+            var skusArray = this.props.currentStyleInfo.skus
+            var quantityArray = [];
+            for (var key in skusArray) {
+                data.push({sku: key, quantity: skusArray[key].quantity, size: skusArray[key].size})
             }
+            console.log('IMPORTANT DATA', this.props.currentStyleInfo.skus, data[0])
+            for (var i = 1; i <= this.props.currentStyleInfo.skus[data[0].sku].quantity; i++) {
+                quantityArray.push(i)
+            }
+            if (quantityArray.length > 15) {
+                quantityArray.length = 15;
+            }
+            this.setState({
+                size: this.props.currentStyleInfo.skus[data[0].sku].size,
+                quantity: 1,
+                totalQuantity: this.props.currentStyleInfo.skus[data[0].sku].quantity,
+                quantityArr: quantityArray,
+                data: data,
+                currentSku: data[0].sku
+            })
+            //if state is changed by sizeSelector
+        } else if (this.state.flag === true) {
+            console.log('second part of componentDidUpdate')
+            var newQuantityTotal = 0
+            var newQuantityArray = []
+            this.setState({
+                flag: false
+            })
+
         }
     }
     //Dynamically changes selected option
@@ -93,12 +93,11 @@ export default class AddToCart extends React.Component {
 
     render() {
         //loading information for data, so that component can render without data if need be. 
-
+        console.log('PROPS IN ADDTOCART', this.props);
         return (
             <div className="addToCart">
                 Size: <select name="size" onChange={this.changeData} >
                     {this.state.data.map((item) => {
-                        console.log(item)
                         return (
                             <option value={`${item.size}, ${item.sku}`} key={item.sku}>{item.size}</option>
                         )
